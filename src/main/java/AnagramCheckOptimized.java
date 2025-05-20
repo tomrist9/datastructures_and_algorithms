d007bf66e131daea7bde1af9210c2f8c747f71ac
tomrist9 <tomristt9@gmail.com>
2025-05-20 13:32:17 +0400

    consider maintaining the frequency of each character. We can do this by having two separate hash tables for the two strings. Then, we can check whether the frequency of each character in string s1 is equal to that in string s2 and vice versa.

--- src/main/java/AnagramCheckOptimized.java
+++ src/main/java/AnagramCheckOptimized.java
@@ -2,24 +2,29 @@ package main.java;
 
 
 public class AnagramCheckOptimized {
-    public static boolean isAnagram(String s1, String s2){
-        if(s1.length()!=s2.length()){
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
             return false;
         }
-        int[] charCount =new int[26];
+        int[] charCount = new int[26];
 
-        for(char c:s1.toCharArray()){
-            charCount[c-'a']++;
+        for (char c : s1.toCharArray()) {
+            if (Character.isLetter(c)) {
+                charCount[c - 'a']++;
+            }
         }
-        for(char c:s2.toCharArray()){
-            charCount[c-'a']++;
+        for (char c : s2.toCharArray()) {
+            if (Character.isLetter(c)) {
+                charCount[c - 'a']++;
+            }
         }
-        for(int count:charCount){
-            if(count!=0){
+        for (int count : charCount) {
+            if (count != 0) {
                 return false;
             }
         }
         return true;
     }
 
+}
 }
