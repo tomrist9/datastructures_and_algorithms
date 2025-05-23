25f9355fd655b1c7a6c52f625c4ae0f7bd81b711
tomrist9 <tomristt9@gmail.com>
2025-05-23 15:08:16 +0400

    groupanagrams task

---
+++ src/main/java/GroupAnagrams.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class GroupAnagrams {
+    //Group Anagrams
+    //Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+    //
+    //An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+}
