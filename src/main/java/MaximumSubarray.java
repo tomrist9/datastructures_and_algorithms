9b1a06f55761e8b0459f047364e7deeee03cb97e
tomrist9 <tomristt9@gmail.com>
2025-05-27 13:59:17 +0400

    find maximum sub array with Kadane’s Algorithm

---
+++ src/main/java/MaximumSubarray.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class MaximumSubarray {
+    public static int maxSubArray(int [] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    }
+}
