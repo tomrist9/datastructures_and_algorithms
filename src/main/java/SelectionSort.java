69b7853b32360925d0a9c4940eebfffd9814ede2
tomrist9 <tomristt9@gmail.com>
2025-05-28 13:32:02 +0400

    sorted arrays with sselection sorting algorithm

---
+++ src/main/java/SelectionSort.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            // Find the index of the smallest element in the remaining array
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+    }
+}
