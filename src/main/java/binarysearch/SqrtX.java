8afde5c4ed4b60e5e1ed157e462eb8227299dad2
Tomris Teymurlu <tomristt9@gmail.com>
2025-09-02 12:37:23 +0400

    feat(sqrt): implement integer square root using binary search (iterative)

---
+++ src/main/java/binarysearch/SqrtX.java
@@ -0,0 +1,25 @@
+package main.java.binarysearch;
+
+public class SqrtX {
+    public int mySqrt(int x){
+        if(x<2){
+            return x;
+        }
+
+        int left = 1, right = x/2, ans =0;
+
+        while(left<=right){
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid;
+            if(square == x){
+                return mid;
+            }else if(square < x){
+                left = mid + 1;
+                ans = mid;
+            }else{
+                left = mid -1;
+            }
+        }
+        return ans;
+    }
+}
