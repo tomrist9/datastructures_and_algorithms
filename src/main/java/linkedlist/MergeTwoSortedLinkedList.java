6b1cb1107da5a553d6f257cd0e44daa7deaeadc1
Tomris Teymurlu <tomristt9@gmail.com>
2025-09-20 15:55:16 +0400

    feat: implement mergeTwoLists method using dummy node approach
    - Added ListNode helper setup with dummy node (-1) as placeholder
    - Implemented merge logic with while loop comparing l1 and l2
    - Attached remaining nodes after one list ends
    - Ensured final merged result is returned via dummy.next

---
+++ src/main/java/linkedlist/MergeTwoSortedLinkedList.java
@@ -0,0 +1,26 @@
+package main.java.linkedlist;
+
+public class MergeTwoSortedLinkedList {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) {
+            current.next = l1;
+        }
+        if (l2 != null) {
+            current.next = l2;
+        }
+        return dummy.next;
+    }
+}
