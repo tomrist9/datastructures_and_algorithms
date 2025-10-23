b15adbbe677a1116944f3f7f3469a54589104329
Tomris Teymurlu <tomristt9@gmail.com>
2025-10-23 17:05:54 +0400

    feat: solve permutation string challenge using sliding window and frequency arrays

---
+++ src/main/java/slidingwindow/PermutationString.java
@@ -0,0 +1,22 @@
+package main.java.slidingwindow;
+
+import java.util.Arrays;
+
+public class PermutationString {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) return false;
+        int [] s1Count = new int[26];
+        int [] s2Count = new int[26];
+        for(int i =0; i< s1.length(); i++){
+            s1Count[s1.charAt(i)-'a']++;
+            s2Count[s2.charAt(i)-'a']++;
+        }
+
+        for(int i = s1.length(); i< s2.length(); i++){
+            if(Arrays.equals(s1Count, s2Count)) return true;
+            s2Count[s2.charAt(i)-'a']++;
+            s2Count[s2.charAt(i-s1.length())]--;
+        }
+        return Arrays.equals(s1Count, s2Count);
+    }
+}
