9cf5a25e2ffa0f381763f4dbd2a6ca8b10ef1cb9
Tomris Teymurlu <tomristt9@gmail.com>
2025-11-01 13:53:27 +0400

    feat: add stack-based solution for Largest Rectangle in Histogram (O(n))

---
+++ src/main/java/stack/LargestRectangleinHistogram.java
@@ -0,0 +1,28 @@
+package main.java.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LargestRectangleinHistogram {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int max = 0;
+        Deque<Integer> st = new ArrayDeque<>();
+        st.push(-1);
+
+        for (int i = 0; i <= n; i++) {
+            int curr = (i == n) ? 0 : heights[i];
+
+            while (st.peek() != -1 && curr < heights[st.peek()]) {
+                int mid = st.pop();
+                int left = st.peek();
+
+                int width = i - left - 1;
+                max = Math.max(max, heights[mid] * width);
+            }
+            st.push(i);
+        }
+        return max;
+    }
+
+}
