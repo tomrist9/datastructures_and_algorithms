c4a195fc597d42c24d46c71cbf7daa8e64a5f27c
Tomris Teymurlu <tomristt9@gmail.com>
2025-11-06 13:37:59 +0400

    feat: solve Find Minimum in Rotated Sorted Array (LeetCode #153)

---
+++ src/main/java/binarysearch/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,21 @@
+package main.java.binarysearch;
+
+public class FindMinimuminRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+}
