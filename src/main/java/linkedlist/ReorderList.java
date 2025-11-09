bac4334dddb32ac6448b235f4588aa587bf775c3
Tomris Teymurlu <tomristt9@gmail.com>
2025-11-09 14:55:05 +0400

    feat: implement reorder list using slow-fast pointer and in-place reversal

---
+++ src/main/java/linkedlist/ReorderList.java
@@ -0,0 +1,35 @@
+package main.java.linkedlist;
+
+public class ReorderList {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null, curr = slow.next;
+        slow.next = null;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        ListNode first = head, second = prev;
+        while (second != null) {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+
+            first.next = second;
+            second.next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+
+}
