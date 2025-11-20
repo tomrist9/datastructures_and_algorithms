2f4cbbc2b4d847b36caf05ef84bca4b009b789e5
Tomris Teymurlu <tomristt9@gmail.com>
2025-11-20 13:54:05 +0400

    feat: implement recursive DFS solution to invert binary tree

---
+++ src/main/java/binarysearch/InvertBinaryTree.java
@@ -0,0 +1,17 @@
+package main.java.binarysearch;
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root== null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
