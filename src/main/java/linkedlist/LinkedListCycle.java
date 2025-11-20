e7f8b66b6732a426c42cf5036eda19e48e76d575
Tomris Teymurlu <tomristt9@gmail.com>
2025-11-20 14:23:02 +0400

    feat: implement Floyd’s Tortoise & Hare algorithm to detect linked list cycle

--- src/main/java/linkedlist/LinkedListCycle.java
+++ src/main/java/linkedlist/LinkedListCycle.java
@@ -18,4 +18,16 @@ public class LinkedListCycle {
         }
         return false;
     }
+
+    public boolean hasCycleOptimizedSolution(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
 }
