b600c8c0a967ba1f8c14b9189fca6fe07b53e7b4
Tomris Teymurlu <tomristt9@gmail.com>
2025-11-22 12:38:17 +0400

    feat: implement O(n) DFS solution for Balanced Binary Tree

---
+++ src/main/java/binarysearch/BalancedBinaryTree.java
@@ -0,0 +1,21 @@
+package main.java.binarysearch;
+
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = dfs(node.left);
+        if (left == -1) return -1;
+
+        int right = dfs(node.right);
+        if (right == -1) return -1;
+
+        if (Math.abs(left - right) > 1) return -1;
+
+        return 1 + Math.max(left, right);
+    }
+}
