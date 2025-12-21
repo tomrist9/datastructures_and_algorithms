a2ad413bd45eeed17d1469c02bb9548ef5a5502a
tomrist9 <tomristt9@gmail.com>
2025-12-21 23:10:40 +0400

    feat: implement subsets using backtracking

---
+++ src/main/java/backtracking/Subsets.java
@@ -0,0 +1,26 @@
+package main.java.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(0, nums, new ArrayList<>());
+        return result;
+    }
+
+    private void backtrack(int start, int[] nums, List<Integer> path) {
+        // Hal-hazırkı kombinasiya nəticədir
+        result.add(new ArrayList<>(path));
+
+        // Növbəti elementləri seç
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);              // seç
+            backtrack(i + 1, nums, path);   // davam et
+            path.remove(path.size() - 1);   // geri qayıt (backtrack)
+        }
+    }
+}
