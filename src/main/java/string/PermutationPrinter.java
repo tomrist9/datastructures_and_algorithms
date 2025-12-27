69d618f972b18ba57d2e6be94e971fc9b8fd9ba0
tomrist9 <tomristt9@gmail.com>
2025-12-27 13:38:37 +0400

    feat: implement recursive solution for Permutation in Strings

---
+++ src/main/java/string/PermutationPrinter.java
@@ -0,0 +1,19 @@
+package main.java.string;
+
+public class PermutationPrinter {
+    public void permute(String str){
+        permutation(str, "");
+    }
+
+    public void permutation(String str, String prefix){
+        if(str.length()==0){
+            System.out.println(prefix);
+            return;
+        }
+
+        for(int i=0; i<str.length(); i++){
+            String rem = str.substring(0,i)+ str.substring(i+1);
+            permutation(rem, prefix + str.charAt(i));
+        }
+    }
+}
