58dd4d69ac16d63b2f526e272414a70977a3945c
tomrist9 <tomristt9@gmail.com>
2026-01-13 23:15:01 +0400

    feat: add deep copy implementation for graph cloning using DFS

---
+++ src/main/java/tree_graph/CloneGraph.java
@@ -0,0 +1,48 @@
+package main.java.tree_graph;
+
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CloneGraph {
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node copies = new Node(node.val);
+        visited.put(node, copies);
+
+        for (Node neighbor : node.neighbors) {
+            copies.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return copies;
+    }
+}
