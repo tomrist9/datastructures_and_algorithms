12f9fd311ee23f552e9a664e7b19c546682c3b01
tomrist9 <tomristt9@gmail.com>
2026-01-16 14:39:47 +0400

    feat: solve Subarray Sum Equals K using prefix sum and hashmap

---
+++ src/main/java/array/SubarraySumEqualsK.java
@@ -0,0 +1,23 @@
+package main.java.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int num : nums){
+            sum+=num;
+
+            if(map.containsKey(sum-k)){
+                count += map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+
+        return count;
+    }
+}
