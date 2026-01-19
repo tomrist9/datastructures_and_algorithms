c6aa48c41bf6498b0b8e659b197637b3809ec1c6
tomrist9 <tomristt9@gmail.com>
2026-01-19 23:53:12 +0400

    feat: solve Surrounded Regions using multi source BFS

---
+++ src/main/java/tree_graph/SurroundedRegions.java
@@ -0,0 +1,78 @@
+package main.java.tree_graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SurroundedRegions {
+    private static final int[][] DIRS = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+    };
+
+    public void solve(char[][] board) {
+
+        if (board == null || board.length <= 1 || board[0].length <= 1) return;
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+
+        for (int r = 0; r < rows; r++) {
+            if (board[r][0] == 'O') {
+                board[r][0] = '#';
+                queue.offer(new int[]{r, 0});
+            }
+            if (board[r][cols - 1] == 'O') {
+                board[r][cols - 1] = '#';
+                queue.offer(new int[]{r, cols - 1});
+            }
+        }
+
+
+        for (int c = 0; c < cols; c++) {
+            if (board[0][c] == 'O') {
+                board[0][c] = '#';
+                queue.offer(new int[]{0, c});
+            }
+            if (board[rows - 1][c] == 'O') {
+                board[rows - 1][c] = '#';
+                queue.offer(new int[]{rows - 1, c});
+            }
+        }
+
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int r = cell[0];
+            int c = cell[1];
+
+            for (int[] d : DIRS) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nr < rows &&
+                        nc >= 0 && nc < cols &&
+                        board[nr][nc] == 'O') {
+
+                    board[nr][nc] = '#';
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (board[r][c] == 'O') {
+                    board[r][c] = 'X';
+                } else if (board[r][c] == '#') {
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+}
