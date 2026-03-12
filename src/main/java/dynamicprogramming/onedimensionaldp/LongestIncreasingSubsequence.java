040dbbae0b7b427c7bb1932bec0b2ef00b621504
tomrist9 <tomristt9@gmail.com>
2026-03-12 14:12:40 +0400

    feat: solve longest increasing subsequence using dynamic programming

---
+++ src/main/java/dynamicprogramming/onedimensionaldp/LongestIncreasingSubsequence.java
@@ -0,0 +1,23 @@
+package main.java.dynamicprogramming.onedimensionaldp;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int max=1;
+
+        for(int i=0; i<n; i++){
+            for(int j =0; j<i; j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=Math.max(dp[i], dp[j]+1);
+                }
+            }
+            max=Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
