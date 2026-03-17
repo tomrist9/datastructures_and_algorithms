b250e0a03b9dcd0850997de643c11381c5d3612b
tomrist9 <tomristt9@gmail.com>
2026-03-17 17:02:47 +0400

    feat(dp): solve Best Time to buy and sell stock with CoolDown with DP (O(1))

---
+++ src/main/java/dynamicprogramming/twodimensionaldp/BestTimeBuySellStockCooldown.java
@@ -0,0 +1,17 @@
+package main.java.dynamicprogramming.twodimensionaldp;
+
+public class BestTimeBuySellStockCooldown {
+    public int maxProfit(int[] prices) {
+        int hold = -prices[0];
+        int sold=0;
+        int rest=0;
+
+        for(int i=1; i<prices.length; i++){
+            int prevSold= sold;
+            sold=hold+prices[i];
+            hold=Math.max(hold, rest-prices[i]);
+            rest=Math.max(rest, prevSold);
+        }
+        return Math.max(rest, sold);
+    }
+}
