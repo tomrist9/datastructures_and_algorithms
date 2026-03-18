0a62f586f6ae24ea4638e6ceb7126ed8e0f6ba82
tomrist9 <tomristt9@gmail.com>
2026-03-18 12:36:06 +0400

    feat: solve highest even number problem with optimal time and space complexity
    - time complexity: O(n)
    - space complexity: O(1)
    - uses conditional max tracking pattern

--- src/main/java/array/HighestEvenNumber.java
+++ src/main/java/array/HighestEvenNumber.java
@@ -5,22 +5,20 @@ import java.util.Set;
 
 public class HighestEvenNumber {
     public static Integer highestEven(int[] arr) {
-        Integer max=null;
+        Integer max = null;
 
-        for(int num : arr){
-        if(num%2==0){
-           if(max==null || num>max){
-               max=num;
-           }
-        }
-
-
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                if (max == null || num > max) {
+                    max = num;
+                }
+            }
         }
         return max;
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,6,8,24, 35, 38, 40, 4};
+        int[] arr = {5, 6, 8, 24, 35, 38, 40, 4};
 
         System.out.println(highestEven(arr));
     }
