fd6a0bb82b706c5fc35baaa66122a9d7278043a6
tomrist9 <tomristt9@gmail.com>
2026-03-30 15:06:19 +0400

    feat: solve Gas Station problem using greedy algorithm with total and tank tracking

---
+++ src/main/java/greedy/GasStation.java
@@ -0,0 +1,21 @@
+package main.java.greedy;
+
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start = 0;
+        int tank =0;
+        int total=0;
+        for(int i=0; i<gas.length; i++){
+            tank+=gas[i]-cost[i];
+            total+=gas[i]-cost[i];
+
+            if(tank<0){
+                start=i+1;
+                tank=0;
+            }
+        }
+
+        return total>=0?start:-1;
+
+    }
+}
