b84cf95014289d7560d5e4d10c4be2576b6d2e56
tomrist9 <tomristt9@gmail.com>
2026-04-13 15:31:10 +0400

    feat: solve Set Matrix Zeroes in-place with constant space using marker technique

---
+++ src/main/java/math/SetMatrixZeroes.java
@@ -0,0 +1,47 @@
+package main.java.math;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int n =matrix.length;
+        int m = matrix[0].length;
+        boolean firstCol = false;
+        boolean firstRow=false;
+
+        for(int i=0; i<n; i++){
+            if(matrix[i][0]==0){
+                firstCol=true;
+            }
+        }
+
+        for(int j=0; j<m; j++){
+            if(matrix[0][j]==0){
+                firstRow=true;
+            }
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(firstCol){
+            for(int i=0; i<n; i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(firstRow){
+            for(int j=0; j<m; j++){
+                matrix[0][j]=0;
+            }
+        }
+    }
+}
