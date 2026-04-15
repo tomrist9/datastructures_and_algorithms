8124ad9b460a329fe5e00c6a53f1d671ca8f9725
tomrist9 <tomristt9@gmail.com>
2026-04-15 16:00:57 +0400

    feat: solve Happy Number problem using HashSet for cycle detection

--- src/main/java/math/HappyNumber.java
+++ src/main/java/math/HappyNumber.java
@@ -1,26 +1,27 @@
 package main.java.math;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
 
     public boolean isHappy(int n) {
-
-        int slow = n;
-        int fast = getSumOfSquares(n);
-        if (fast != 1 && slow != fast) {
-            slow = getSumOfSquares(slow);
-            fast = getSumOfSquares(getSumOfSquares(fast));
+        Set<Integer> set = new HashSet<>();
+        while(n!=1 && !set.contains(n)){
+            set.add(n);
+            n= getSumOfSquares(n);
         }
-
-        return fast == 1;
+        return n==1;
     }
 
-    public int getSumOfSquares(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n = n / 10;
+    private int getSumOfSquares(int n){
+        int sum =0;
+        while(n>0){
+            int digit = n%10;
+            sum+=digit * digit;
+            n= n/10;
         }
+
         return sum;
     }
 }
