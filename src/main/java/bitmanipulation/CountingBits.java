1bbbfcc07baaf059d26fd74afc1d924f793f603a
tomrist9 <tomristt9@gmail.com>
2026-04-25 14:40:34 +0400

    feat: add solution for Counting Bits problem using bit manipulation

---
+++ src/main/java/bitmanipulation/CountingBits.java
@@ -0,0 +1,12 @@
+package main.java.bitmanipulation;
+
+public class CountingBits {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        res[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+}
