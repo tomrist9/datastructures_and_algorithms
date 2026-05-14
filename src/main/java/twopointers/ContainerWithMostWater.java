6f8294568bd9d9ae6f73abb67fac4b94e0fb186c
tomrist9 <tomristt9@gmail.com>
2026-05-14 14:51:56 +0400

    feat: solve Container with most water two pointers approach

--- src/main/java/twopointers/ContainerWithMostWater.java
+++ src/main/java/twopointers/ContainerWithMostWater.java
@@ -2,25 +2,24 @@ package main.java.twopointers;
 
 public class ContainerWithMostWater {
     public int maxArea(int[] heights) {
-        int left = 0;
-        int right = heights.length - 1;
-        int mostWater = 0;
+            int left =0;
+            int right = heights.length -1;
+            int max=0;
+            while(left<right){
+                int heightt = Math.min(heights[left], heights[right]);
+                int width= right-left;
+                int area=width*heightt;
+                max = Math.max(max, area);
+                if(heights[left]<heights[right]){
 
-        while (left < right) {
-            int width = right - left;
-            int containerHeight = Math.min(heights[left], heights[right]);
+                    left++;
+                }else{
+                    right--;
+                }
 
-            int water = width * containerHeight;
 
-            mostWater = Math.max(mostWater, water);
-
-            if (heights[left] < heights[right]) {
-                left++;
-            } else {
-                right--;
             }
+            return max;
         }
-        return mostWater;
-    }
-}
 
+    }
