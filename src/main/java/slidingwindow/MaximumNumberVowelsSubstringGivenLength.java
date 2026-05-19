70b5123215f72c45002a8d334e86da0574d485a5
tomrist9 <tomristt9@gmail.com>
2026-05-19 12:32:33 +0400

    feat: implement sliding window solution for Maximum number of vowels in substring given length

---
+++ src/main/java/slidingwindow/MaximumNumberVowelsSubstringGivenLength.java
@@ -0,0 +1,35 @@
+package main.java.slidingwindow;
+
+public class MaximumNumberVowelsSubstringGivenLength {
+    public int maxVowels(String s, int k) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                count++;
+            }
+        }
+        max = count;
+
+        for (int i = k; i < s.length(); i++) {
+
+            if (isVowel(s.charAt(i - k))) {
+                count--;
+
+
+            }
+
+            if (isVowel(s.charAt(i))) {
+                count++;
+
+
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i'
+                || c == 'o' || c == 'u';
+    }
+}
