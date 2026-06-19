af7c7a5a10f391fb1bd1e2099f1b45e7c2274ee6
tomrist9 <tomristt9@gmail.com>
2026-06-19 14:38:31 +0400

    feat: add recursive solution for Search in a Binary Search Tree

---
+++ src/main/java/binarysearch/SearchBinarySearchTree.java
@@ -0,0 +1,20 @@
+package main.java.binarysearch;
+
+public class SearchBinarySearchTree {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if(root==null) return null;
+
+            if(root.val==val){
+                return root;
+            }
+
+            if(val<root.val){
+                return searchBST(root.left, val);
+            }
+
+            else{
+                return searchBST(root.right, val);
+
+            }
+        }
+}
