3936a4f5720b347cfdab67aa397aa656fed917b3
tomriteymurlu <tomristt9@gmail.com>
2026-07-14 12:03:00 +0400

    `feat: solve Find Peak Element using binary search`

---
+++ src/main/java/binarysearch/FindPeakElement.java
@@ -0,0 +1,19 @@
+package main.java.binarysearch;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        int left=0;
+        int right = nums.length-1;
+
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]<nums[mid+1]){
+                left=mid+1;
+            }else{
+                right= mid;
+            }
+
+        }
+        return right;
+    }
+}
