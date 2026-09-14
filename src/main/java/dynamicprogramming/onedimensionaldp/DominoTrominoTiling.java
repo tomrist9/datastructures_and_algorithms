fbe9050bfe7ce05e7b84855f42bb250bb7be6e4d
tomriteymurlu <tomristt9@gmail.com>
2026-09-14 21:55:56 +0400

    feat: solve Domino and Tromino Tiling using dynamic programming

---
+++ src/main/java/dynamicprogramming/onedimensionaldp/DominoTrominoTiling.java
@@ -0,0 +1,24 @@
+package main.java.dynamicprogramming.onedimensionaldp;
+
+public class DominoTrominoTiling {
+    private static final int MOD = 1_000_000_007;
+
+    public int numTilings(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 5;
+
+        long[] dp = new long[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+}
